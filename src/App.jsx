1173dc75ff10412e135954ed6338ad395f827ed0
Sammuel Ramclief Isaac <sisaac@uespoir.com>
2026-03-14 11:14:31 -0700

    Started working on the todo app

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,59 @@ import './App.css'
 // 1
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    title: "",
+    desc: "",
+    isCompleted: false,
+  })
+
+  const [taskList, setTaskList] = useState()
+
+  const  handleChnage = (e)=>{
+    setFormData((prev)=>({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const addTask = () =>{
+    // Add task to my list
+    setTaskList((prev)=> ({...prev, [e.target.name]:e.target.value}))
+  }
+  console.log(taskList)
+  
+  const updateTask = () =>{
+    // Update my list
+  }
+
+
 
   return (
     <>
-      
+      <form >
+        <input type="text"
+        name='title'
+        onChange={handleChnage}
+        value={formData.value} />
+        
+        <input type="text"
+        name='desc'
+        onChange={handleChnage}
+        value={formData.value} />
+
+        <div className="todos">
+          <ul>
+          {
+            taskList.map((task, index)=>{
+              <li key={index}>
+                Title: {task.title}
+              </li>
+            })
+          }
+          </ul>
+        </div>
+
+      </form>
     </>
   )
 }
